c77b1c8126f6e02977c3eb7e2108424ad5565f8a
lzy <3312202467@qq.com>
2026-05-24 22:10:10 +0800

    refactor(Home.vue): 优化Home页面代码格式并添加微前端展示文本
    1.  统一代码缩进和空格规范，修复语法格式问题
    2.  移除空的style标签并简化样式写法
    3.  添加"微前端"文本展示到页面中
    4.  修正data和方法中的空格格式问题

--- main/src/views/Home.vue
+++ main/src/views/Home.vue
@@ -5,30 +5,31 @@
     </div>
     <div>
       <!-- 这里通过data标签发送数据是同步的，但是子应用的加载是异步的 -->
-      <micro-app :data="data" @datachange="handleSubData"  name="app" url="http://localhost:4002" destroy></micro-app>
+      <micro-app :data="data" @datachange="handleSubData" name="app" url="http://localhost:4002" destroy></micro-app>
     </div>
 
-    <HelloWorld :msg="msg"/>
+    <HelloWorld :msg="msg" />
+    <div>微前端</div>
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App',
       data: {
         name: 'micro-app',
-        type:'微前端'
+        type: '微前端'
       }
     }
   },
-  mounted() { 
+  mounted() {
     setTimeout(() => {
       this.data = {
         name: '---来自基座应用的数据---',
-        type:'微前端'
+        type: '微前端'
       }
     }, 2000);
   },
@@ -36,17 +37,14 @@ export default {
     HelloWorld
   },
   methods: {
-    showGlobalStr() { 
+    showGlobalStr() {
       alert(window.globalStr)
     },
-    handleSubData(e) { 
+    handleSubData(e) {
       console.log('基座接收数据:', e.detail.data);
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
-
+<style scoped></style>
